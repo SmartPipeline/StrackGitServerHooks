71266c06c72e629e321257664bf58b568bfb78df
Aaron <guoliangxu1987@163.com>
2019-02-22 21:13:49 +0800

    refract

--- web_hooks/GiteePullRequest.py
+++ web_hooks/GiteePullRequest.py
@@ -1,40 +1,55 @@
 # coding=utf8
 # Copyright (c) 2019 CineUse
 
-from flask_restful import reqparse, Resource
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('action', type=str)
-parser.add_argument('pull_request', type=dict)
-parser.add_argument('number', type=int)
-parser.add_argument('iid', type=int)
-parser.add_argument('title', type=str)
-parser.add_argument('state', type=str)
-parser.add_argument('merge_status', type=str)
-parser.add_argument('merge_commit_sha', type=str)
-parser.add_argument('url', type=str)
-parser.add_argument('source_branch', type=str)
-parser.add_argument('source_repo', type=dict)
-parser.add_argument('target_branch', type=str)
-parser.add_argument('target_repo', type=dict)
-parser.add_argument('project', type=dict)
-parser.add_argument('repository', type=dict)
-parser.add_argument('author', type=dict)
-parser.add_argument('updated_by', type=dict)
-parser.add_argument('sender', type=dict)
-parser.add_argument('target_user', type=dict)
-parser.add_argument('hook_name', type=str)
-parser.add_argument('password', type=str)
+from flask_restplus import Resource
+from utils.get_arg_parser import get_arg_parser
+from utils.parse_args import parse_args
+from utils.get_strack_api import get_strack_api
 
 
 class GiteePullRequest(Resource):
-    st = None
-    password = None
+    arg_list = [
+        {'name': 'action', 'type': str, 'default': '', 'required': False},
+        {'name': 'pull_request', 'type': dict, 'default': '', 'required': False},
+        {'name': 'number', 'type': int, 'default': '', 'required': False},
+        {'name': 'iid', 'type': int, 'default': '', 'required': False},
+        {'name': 'title', 'type': str, 'default': '', 'required': False},
+        {'name': 'state', 'type': str, 'default': '', 'required': False},
+        {'name': 'merge_status', 'type': str, 'default': '', 'required': False},
+        {'name': 'merge_commit_sha', 'type': str, 'default': '', 'required': False},
+        {'name': 'url', 'type': str, 'default': '', 'required': False},
+        {'name': 'source_branch', 'type': str, 'default': '', 'required': False},
+        {'name': 'source_repo', 'type': dict, 'default': '', 'required': False},
+        {'name': 'target_branch', 'type': str, 'default': '', 'required': False},
+        {'name': 'target_repo', 'type': dict, 'default': '', 'required': False},
+        {'name': 'project', 'type': dict, 'default': '', 'required': False},
+        {'name': 'repository', 'type': dict, 'default': '', 'required': False},
+        {'name': 'author', 'type': dict, 'default': '', 'required': False},
+        {'name': 'updated_by', 'type': dict, 'default': '', 'required': False},
+        {'name': 'sender', 'type': dict, 'default': '', 'required': False},
+        {'name': 'target_user', 'type': dict, 'default': '', 'required': False},
+        {'name': 'hook_name', 'type': str, 'default': '', 'required': False},
+        {'name': 'password', 'type': str, 'default': '', 'required': False},
+    ]
+
+    parser = get_arg_parser(arg_list)
+
+    st_info = {
+        'url': 'http://129.204.29.79:88/strack',
+        'login': 'gitee',
+        'passwd': 'gitee2Strack'
+    }
+
+    password = 'CvIkY1V73fi5ikU4'
+    url = '/strack_hook'
+
+    @property
+    def st(self):
+        return get_strack_api(self.st_info.get('url'), self.st_info.get('login'), self.st_info.get('passwd'))
 
     def post(self):
         # 解析payload
-        args = parser.parse_args()
+        args = parse_args(self.parser)
         #
         return args.get('action'), 201
 
